d1b0735bbe4046a359791c60801dadfac938834f
takayoshiiiiii <takayoshiiiiii@users.noreply.github.com>
2020-03-27 20:31:29 +0900

    [fix] bug fix

--- setup_pip.sh
+++ setup_pip.sh
@@ -1,10 +1,10 @@
 if type "pyenv" > /dev/null 2>&1; then
-  echo "pyenv is not installed."
-  exit 1
-else
   pyenv install -s 3.6.5
   pyenv virtualenv 3.6.5 default_env
   pyenv global default_env
   pip install pip setuptools -U
   pip install powerline-shell flake8 neovim
+else
+  echo "pyenv is not installed."
+  exit 1
 fi
